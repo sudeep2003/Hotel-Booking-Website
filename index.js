de1c13438d93b72b227d13ee18b1cfc10d312e7f
sudeep2003 <86313181+sudeep2003@users.noreply.github.com>
2023-07-14 12:50:18 -0700

    add readMe

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get("/",(req,res)=>{
     res.sendFile(`${__dirname}/pages/index.html`);
 })
 
+app.get("/rooms",(req,res)=>{
+    res.sendFile(`${__dirname}/pages/rooms.html`);
+})
+app.get("/blog",(req,res)=>{
+    res.sendFile(`${__dirname}/pages/blog.html`);
+})
+app.get("/contact",(req,res)=>{
+    res.sendFile(`${__dirname}/pages/contact.html`);
+})
+
 app.listen(3000,()=>{
     console.log("This is running on port 3000.");
 })
