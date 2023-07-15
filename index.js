26bc0e9a37c2ae9967f679698732ef02ed045793
sudeep2003 <86313181+sudeep2003@users.noreply.github.com>
2023-07-15 12:16:03 -0700

    Changes on index.js for rendering ejs pages

--- index.js
+++ index.js
@@ -11,23 +11,25 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get("/reservation",(req,res)=>{
-    res.sendFile(`${__dirname}/pages/reservation.html`);
-})
+app.get('/rooms', (req, res) => {
+    res.render('rooms');
+});
 
-app.get("/rooms",(req,res)=>{
-    res.sendFile(`${__dirname}/pages/rooms.html`);
-})
-app.get("/blog",(req,res)=>{
-    res.sendFile(`${__dirname}/pages/blog.html`);
-})
-app.get("/contact",(req,res)=>{
-    res.sendFile(`${__dirname}/pages/contact.html`);
-})
-app.get("/about-us",(req,res)=>{
-    app.sendFile(`${__dirname}/pages/about-us.html`);
+app.get('/blog', (req, res) => {
+    res.render('blog');
+});
 
-})
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/about-us', (req, res) => {
+    res.render('about-us');
+});
+
+app.get('/reservation', (req, res) => {
+    res.render('reservation');
+});
 
 app.listen(3000,()=>{
     console.log("This is running on port 3000.");
