aa0734371176946a0c7b26583b625c450bacde1e
sudeep2003 <86313181+sudeep2003@users.noreply.github.com>
2023-07-15 11:45:03 -0700

    Adding ejs

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+//jshint esversion:6
+
 const express = require("express");
 
 const app = express();
 
 app.use(express.static('public'))
+app.set('view engine', 'ejs');
 
-app.get("/",(req,res)=>{
-    res.sendFile(`${__dirname}/pages/index.html`);
-})
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 app.get("/rooms",(req,res)=>{
     res.sendFile(`${__dirname}/pages/rooms.html`);
