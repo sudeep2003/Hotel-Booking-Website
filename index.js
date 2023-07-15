a742e6ab6b14aac2041677f28426ce20e376fd68
Aritra <banikaritra5@outlook.com>
2023-07-15 23:58:07 +0530

    adding the reservation.html

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/",(req,res)=>{
     res.sendFile(`${__dirname}/pages/index.html`);
 })
 
+app.get("/reservation",(req,res)=>{
+    res.sendFile(`${__dirname}/pages/reservation.html`);
+})
+
 app.listen(3000,()=>{
     console.log("This is running on port 3000.");
 })
