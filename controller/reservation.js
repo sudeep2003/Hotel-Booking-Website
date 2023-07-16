54ac1ca3fd9672c5fc7b7d67da36d4465ef67483
sudeep2003 <86313181+sudeep2003@users.noreply.github.com>
2023-07-16 12:16:17 -0700

    Change index.js to app.js

--- controller/reservation.js
+++ controller/reservation.js
@@ -1,11 +1,10 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/reservation",(req,res)=>{
+function getFormData(req){
     const firstName = req.body.first_name;
     const lastName = req.body.last_name;
     const email = req.body.email;
     const phone = req.body.phone;
-})
 
+    return{firstName, lastName, email, phone};
+}
+
+module.exports = getFormData;
