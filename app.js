e7459aa22d2dfd60308379a8ef52921cd51393ec
Aritra <banikaritra5@outlook.com>
2023-07-18 21:09:51 +0530

    2nd Commit

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 
 import express from 'express';
 import { index } from './controller/index.js';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 const app = express();
 
 app.use(express.static('public'))
+app.use(cookieParser());
+app.use(session({secret: "keyboard cat", resave: true, saveUninitialized: true, cookie: { secure: true }}));
 app.set('view engine', 'ejs');
 
 app.get('/', index);
--- controller/index.js
+++ controller/index.js
@@ -1,16 +1,17 @@
-// const express = require('express');
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+// var cookieParser = require('cookie-parser');
+// var session = require('express-session');
 
 const app = express();
 
 export function index(req, res) {
-//   const check_in = req.body.check_in;
-//   const check_out = req.body.check_out;
+  const check_in = req.body.check_in;
+  const check_out = req.body.check_out;
 
-  res.render('index',
-//     {
-//     check_in,
-//     check_out
-//   }
-  );
+  req.session.check_in = check_in
+  req.session.check_out = check_out
+
+  res.render('index');
 }
