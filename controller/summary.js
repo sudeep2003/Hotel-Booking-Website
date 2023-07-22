0dc16b1fa28f934638290dfd36e1bbeebd89dbc3
Aritra <banikaritra5@outlook.com>
2023-07-23 01:46:22 +0530

    13th Commit

--- controller/summary.js
+++ controller/summary.js
@@ -3,16 +3,32 @@ import express from 'express';
 const app = express();
 
 export function summary(req, res){
+    console.log(req.session);
     const reservationData = req.session.reservationData;
-    console.log(reservationData);
+    // console.log(reservationData);
+
+    // const firstName = req.session.firstName;
+
+    // const firstName= req.session.firstName;
+    // const lastName = req.session.lastName;
+    // const Name = `${firstName} ${lastName}`;
+    // const email = req.session.email;
+    // const Phone = req.session.phone;
+
     const firstName= reservationData.firstName;
     const lastName = reservationData.lastName;
-    const Name= firstName.concat(' ',lastName);
-    const emailId = reservationData.email;
-    const Phone = reservationData.Phone;
+    const Name = `${firstName} ${lastName}`;
+    const email = reservationData.email;
+    const Phone = reservationData.phone;
 
     const Room = "Family-room";
-    const arrivalTime = 19;
-    const Departure = 20;
-    res.render('reservation_summary',{name:Name, emailid:emailId,phno:Phone});
+    var startDate = req.session.check_in;
+    var endDate = req.session.check_out;
+    res.render('reservation_summary',{
+        Name: Name,
+        startDate: startDate,
+        endDate: endDate,
+        email: email,
+        Phone: Phone
+    });
 }
