9f4817b2259814dd50d211ca8ac7a335fe3dbb6f
sudeep2003 <86313181+sudeep2003@users.noreply.github.com>
2023-07-30 08:22:52 -0700

    Changes

--- database/rooms.js
+++ database/rooms.js
@@ -4,35 +4,13 @@ import {Room} from '../models/models.js'
 
 const app = express();
 
-export default async function availableRooms(arrivalDate, departureDate){
-
-    const Rooms_type = {
-        "Premium King Room": 1,
-        "Deluxe Room": 1,
-        "Double Room": 1,
-        "Luxury Room": 1,
-        "Room With View": 1,
-        "Small View": 1
-    };
-
-    console.log(Rooms_type)
-
-    const UnavailableRooms = await Room.find({
-        unavailableFrom: { $gte: arrivalDate },
-        unavailableTo: { $lte: departureDate }
-    });
-
-    for (const room of UnavailableRooms) {
-        Rooms_type[room.roomName]--;
-    }
-
-    for (const key in Rooms_type) {
-        if (Rooms_type[key] <= 0) {
-            delete Rooms_type[key];
-        }
-    }
-
-    const Rooms_array = Object.keys(Rooms_type);
-    console.log(Rooms_array)
-    return Rooms_array;
+export default function rooms_type(){
+    return({
+    "Premium King Room": 1,
+    "Deluxe Room": 1,
+    "Double Room": 1,
+    "Luxury Room": 1,
+    "Room With View": 1,
+    "Small View": 1
+})
 }
