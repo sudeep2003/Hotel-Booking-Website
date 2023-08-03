f81342006fbf6343ae08fc1fd5b514bf09855820
sudeep2003 <86313181+sudeep2003@users.noreply.github.com>
2023-08-03 13:10:20 -0700

    Changes in avalable rooms.

--- database/roomsAvailable.js
+++ database/roomsAvailable.js
@@ -1,36 +1,35 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import {Room} from '../models/models.js'
 import rooms_type from './rooms.js';
-
+import { Room, RoomRestriction } from '../models/models.js';
 
 const app = express();
 
-export async function availableRooms(arrivalDate, departureDate){
+// export async function availableRooms(arrivalDate, departureDate){
 
-    const Rooms_type = rooms_type();
+//     const Rooms_type = rooms_type();
 
-    console.log(Rooms_type)
+//     console.log(Rooms_type)
 
-    const UnavailableRooms = await Room.find({
-        unavailableFrom: { $gte: arrivalDate },
-        unavailableTo: { $lte: departureDate }
-    });
+//     const UnavailableRooms = await Room.find({
+//         unavailableFrom: { $gte: arrivalDate },
+//         unavailableTo: { $lte: departureDate }
+//     });
 
-    for (const room of UnavailableRooms) {
-        Rooms_type[room.roomName]--;
-    }
+//     for (const room of UnavailableRooms) {
+//         Rooms_type[room.roomName]--;
+//     }
 
-    for (const key in Rooms_type) {
-        if (Rooms_type[key] <= 0) {
-            delete Rooms_type[key];
-        }
-    }
+//     for (const key in Rooms_type) {
+//         if (Rooms_type[key] <= 0) {
+//             delete Rooms_type[key];
+//         }
+//     }
 
-    const Rooms_array = Object.keys(Rooms_type);
-    console.log(Rooms_array)
-    return Rooms_array;
-}
+//     const Rooms_array = Object.keys(Rooms_type);
+//     console.log(Rooms_array)
+//     return Rooms_array;
+// }
 
 export async function availableSingleRoom(singleRoom,arrivalDate, departureDate){
 
@@ -54,4 +53,17 @@ export async function availableSingleRoom(singleRoom,arrivalDate, departureDate)
         console.log(`${Single_room} is not available.`);
         return true;
     }
+}
+
+export async function availableRooms(arrivalDate, departureDate){
+    const roomsData = []
+    const Unavailable_RoomRestriction = await RoomRestriction.find({
+        unavailableFrom: { $gte: arrivalDate },
+        unavailableTo: { $lte: departureDate }
+    });
+    for (const roomRestriction of Unavailable_RoomRestriction) {
+        roomsData = await Room.find({ _id: { $ne: roomRestriction.roomId } });
+    }
+    console.log(roomsData)
+    return roomsData;
 }
